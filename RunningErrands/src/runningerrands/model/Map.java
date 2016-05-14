aef93904b1ba798e6c7f2f6f28bcd6096341d0cc
Katelynn Burkart <kburkart@simplifile.com>
2016-05-14 09:43:56 -0700

    Added Map.java

---
+++ RunningErrands/src/runningerrands/model/Map.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package runningerrands.model;
+
+/**
+ *
+ * @author kburkart
+ */
+public class Map {
+    
+    private int rowCount;
+    private int columnCount;
+    
+    public int getRowCount() {
+        return rowCount;
+    }
+    
+    public int getColumnCount() {
+        return columnCount;
+    }
+    
+    public void setRowCount(int rowCount) {
+        this.rowCount = rowCount;
+    }
+    
+    public void setColumnCount(int columnCount) {
+        this.columnCount = columnCount;
+    }
+}
