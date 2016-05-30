48d7d1cd00b4b0a8b6f033a170dc3b75ad50e538
Katelynn Burkart <kburkart@simplifile.com>
2016-05-30 15:22:42 -0600

    Add functionality and JUnit tests for MoneyControl methods

--- RunningErrands/src/runningerrands/controller/MoneyControl.java
+++ RunningErrands/src/runningerrands/controller/MoneyControl.java
@@ -5,6 +5,7 @@
  */
 package runningerrands.controller;
 
+import java.util.Set;
 import runningerrands.model.Player;
 
 /**
@@ -13,15 +14,58 @@ import runningerrands.model.Player;
  */
 public class MoneyControl {
     
-    public void addToAccountBalance(double accountBalance, double amount) {
+    private static final double JANITOR_WAGE = 7.75;
+    private static final double MAIL_CLERK_WAGE = 10.75;
+    private static final double PIZZA_DELIVERER_WAGE = 7.5;
+    private static final double MECHANIC_WAGE = 12.5;
+    private static final double SECRETARY_WAGE = 11;
+    private static final double NURSE_WAGE = 20.25;
+    private static final double HAIR_DRESSER_WAGE = 13.5;
+    private static final double TEACHER_WAGE = 17.75;
+    
+    
+    public void addToAccountBalance(Player player, double amount) {
+        player.setAccountBalance(player.getAccountBalance() + amount);
+        
     }
     
-    public void subtractFromAccountBalance (double accountBalance, double amount){
+    public void subtractFromAccountBalance (Player player, double amount){
+        player.setAccountBalance(player.getAccountBalance() - amount);
     }
     
-    public double calculatePaycheck (Player player, double hoursWorked) {
-    
-    return 0;
+    public double calculatePaycheck (Player player, double hoursWorked){
+        double paycheckAmount = 0;
+        
+        if (hoursWorked < 0 | hoursWorked > 40) {
+            paycheckAmount = 0;
+        }
+        else {
+             if (player.getJob().equals("janitor")) {
+                paycheckAmount = hoursWorked * JANITOR_WAGE;
+            }
+            else if (player.getJob().equals("mail clerk")) {
+                paycheckAmount = hoursWorked * MAIL_CLERK_WAGE;
+            }
+            else if (player.getJob().equals("pizza deliverer")) {
+                paycheckAmount = hoursWorked * PIZZA_DELIVERER_WAGE;
+            }
+            else if (player.getJob().equals("mechanic")) {
+                paycheckAmount = hoursWorked * MECHANIC_WAGE;
+            }
+            else if (player.getJob().equals("secretary")) {
+                paycheckAmount = hoursWorked * SECRETARY_WAGE;
+            }
+            else if (player.getJob().equals("nurse")) {
+                paycheckAmount = hoursWorked * NURSE_WAGE;
+            }
+            else if (player.getJob().equals("hair dresser")) {
+                paycheckAmount = hoursWorked * HAIR_DRESSER_WAGE;
+            }
+            else if (player.getJob().equals("teacher")) {
+                paycheckAmount = hoursWorked * TEACHER_WAGE;
+            }
+        }    
+        return paycheckAmount;
     }
     
 }
