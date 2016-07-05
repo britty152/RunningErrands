cb66b6a83787eb390662274d68748ffb7b94facf
brittanyhuntington <brittanyhuntington@192.168.0.2>
2016-07-05 16:17:22 -0400

    Lesson 10 Team Assignment

---
+++ RunningErrands/src/exceptions/TravelControlException.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+/**
+ *
+ * @author brittanyhuntington
+ */
+public class TravelControlException extends Exception {
+
+    public TravelControlException() {
+    }
+
+    public TravelControlException(String message) {
+        super(message);
+    }
+
+    public TravelControlException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TravelControlException(Throwable cause) {
+        super(cause);
+    }
+
+    public TravelControlException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+    
+}
